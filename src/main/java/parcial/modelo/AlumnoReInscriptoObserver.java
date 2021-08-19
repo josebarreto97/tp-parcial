7628c0835a3d49a1efb0c42ee7f332a668298ef8
josebarreto97 <joseb1997@hotmail.com>
2021-08-19 18:20:35 -0300

    AlumnoReInscriptoObserver

--- src/main/java/parcial/modelo/AlumnoReInscriptoObserver.java
+++ src/main/java/parcial/modelo/AlumnoReInscriptoObserver.java
@@ -3,23 +3,18 @@ package parcial.modelo;
 public class AlumnoReInscriptoObserver implements Observer{
     private BandejaNotificaciones bandeja;
 
-    // TODO
     @Override
     public void update(Observable observable, Object oldValue, Object newValue) {
         //Si el alumno es reinscripto, crear notificacion
         Alumno alumno = (Alumno) observable;
 
-        if(alumno.getEstado().equals(newValue)){
-            Estado oldSale = (Estado) oldValue;
-            Estado newSale = (Estado) newValue;
+        if(alumno.getEstado().equals(newValue) && newValue instanceof ReInscripto) {
+
+            this.bandeja.crearNotificacion(
+                    "Reinscripcion del alumno " + alumno.getNombre() + " " + alumno.getApellido(),
+                    "El alumno " + alumno.getNombre() + " " + alumno.getApellido() +
+                            " fue reinscripto en el establecimiento."
+            );
         }
-
-        this.bandeja.crearNotificacion(
-                "Reinscripcion del alumno " + alumno.getNombre() + " " + alumno.getApellido(),
-                "El alumno " + alumno.getNombre() + " " + alumno.getApellido() +
-                        " fue reinscripto " + oldSale.getEstado() +
-                        " a " + newSale.getEstado()
-        );
-
     }
 }
