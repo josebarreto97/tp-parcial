9b0fa0c9bb27c730220fbcddf8d024f2c67365e4
josebarreto97 <joseb1997@hotmail.com>
2021-08-19 17:55:05 -0300

    Implementacion parcial de AlumnoReInscriptoObserver

--- src/main/java/parcial/modelo/AlumnoReInscriptoObserver.java
+++ src/main/java/parcial/modelo/AlumnoReInscriptoObserver.java
@@ -6,6 +6,20 @@ public class AlumnoReInscriptoObserver implements Observer{
     // TODO
     @Override
     public void update(Observable observable, Object oldValue, Object newValue) {
+        //Si el alumno es reinscripto, crear notificacion
+        Alumno alumno = (Alumno) observable;
+
+        if(alumno.getEstado().equals(newValue)){
+            Estado oldSale = (Estado) oldValue;
+            Estado newSale = (Estado) newValue;
+        }
+
+        this.bandeja.crearNotificacion(
+                "Reinscripcion del alumno " + alumno.getNombre() + " " + alumno.getApellido(),
+                "El alumno " + alumno.getNombre() + " " + alumno.getApellido() +
+                        " fue reinscripto " + oldSale.getEstado() +
+                        " a " + newSale.getEstado()
+        );
 
     }
 }
