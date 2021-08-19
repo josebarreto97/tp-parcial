d0374da79dfab2f579479bdd58f4443427ff1667
josebarreto97 <joseb1997@hotmail.com>
2021-08-19 17:54:51 -0300

    Implementacion parcial de AlumnoSalidoObserver

--- src/main/java/parcial/modelo/AlumnoSalidoObserver.java
+++ src/main/java/parcial/modelo/AlumnoSalidoObserver.java
@@ -3,9 +3,22 @@ package parcial.modelo;
 public class AlumnoSalidoObserver implements Observer{
     private BandejaNotificaciones bandeja;
 
-    // TODO
     @Override
     public void update(Observable observable, Object oldValue, Object newValue) {
+        //Si el alumno salio, crear notificacion
+        Alumno alumno = (Alumno) observable;
+
+        if(alumno.getEstado().equals(newValue) && newValue.equals()){
+            Estado oldSale = (Estado) oldValue;
+            Estado newSale = (Estado) newValue;
+        }
+
+        this.bandeja.crearNotificacion(
+                "Salida del " + alumno.getNombre() + " " + alumno.getApellido(),
+                "El alumno " + alumno.getNombre() + " " + alumno.getApellido() +
+                        " salio del " + oldSale.getEstado() +
+                        " a " + newSale.getEstado()
+        );
 
     }
 }
