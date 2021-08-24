330658240d18d6f7c85c78ca14dffe6185a8266c
LautaroRobles <lautarorobles_oro@hotmail.com>
2021-08-24 18:55:29 -0300

    Finalizado Menu
    Co-authored-by: josebarreto97 <joseb1997@hotmail.com>

--- src/main/java/parcial/demo/Menu.java
+++ src/main/java/parcial/demo/Menu.java
@@ -36,6 +36,20 @@ public class Menu extends Command{
             {
                 error(scanner,"api", token);
             }
+
+            System.out.println(ANSI_GREEN+" --------- MENU --------- "+ANSI_RESET);
+            System.out.println("Comandos disponibles:");
+            System.out.println(ANSI_YELLOW+"api provincias"+ ANSI_GREEN +" -- Busca provincias"+ANSI_RESET);
+            System.out.println(ANSI_YELLOW+"api municipios [provincia_id]"+ ANSI_GREEN +" -- Busca municipios dada un id de provincia"+ANSI_RESET);
+            System.out.println(ANSI_YELLOW+"api localidades [municipio_id]"+ ANSI_GREEN +" -- Busca localidades dada un id de municipio"+ANSI_RESET);
+            System.out.println("");
+            System.out.println(ANSI_YELLOW+"db get establecimiento [establecimiento_id]"+ ANSI_GREEN +" -- Obtener un establecimiento de la base de datos"+ANSI_RESET);
+            System.out.println(ANSI_YELLOW+"db get alumno [alumno_id]"+ ANSI_GREEN +" -- Obtener un alumno de la base de datos"+ANSI_RESET);
+            System.out.println(ANSI_YELLOW+"db get notificaciones [curso_id]"+ ANSI_GREEN +" -- Obtener las notificaciones del educador de un curso"+ANSI_RESET);
+            System.out.println(ANSI_YELLOW+"db insert alumno [nombre] [apellido] [estado] [curso_id]"+ ANSI_GREEN +" -- Inserta un nuevo alumno en la base de datos"+ANSI_RESET);
+            System.out.println(ANSI_YELLOW+"db promover alumno [alumno_id]"+ ANSI_GREEN +" -- Promover alumno"+ANSI_RESET);
+            System.out.println(ANSI_YELLOW+"db repetir alumno [alumno_id]"+ ANSI_GREEN +" -- Hacer que un alumno repita"+ANSI_RESET);
+            System.out.println(ANSI_YELLOW+"db sacar alumno [alumno_id]"+ ANSI_GREEN +" -- Hacer que un alumno salga"+ANSI_RESET);
         }
     }
 }
